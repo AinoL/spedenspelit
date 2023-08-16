c926ad0502841a2ece0adc918da9a5ec6fb07b9a
Onja <onja@Ainos-MacBook-Pro-2.local>
2023-08-16 20:23:28 +0200

    petals can now be pressed

--- __init__.py
+++ __init__.py
@@ -12,34 +12,54 @@ import random
 # count down from ten seconds
 # when time is up, display score
 
-class Pew(Application):
+class Spede(Application):
     def __init__(self, app_ctx: ApplicationContext) -> None:
         super().__init__(app_ctx)
         self.last_calib = None
         self.petals = [
-            # [37, 5],
-            [5, 13],
-            [13, 21],
-            [21, 29],
-            [29, 37]
+            { "leds":[36, 5], "cap": 0 },
+            { "leds":[4, 13], "cap": 2 },
+            { "leds":[12, 21], "cap": 4 },
+            { "leds":[20, 29], "cap": 6 },
+            { "leds":[28, 37], "cap": 8 }
         ]
+        self.petalid = 0
 
     def draw(self, ctx: Context) -> None:
         # Paint the background black
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
         ctx.rgb(255, 0, 0).round_rectangle(-20, -20, 40, 40, 20).fill()
 
-        for i in range(*self.petals[random.randint(0, 3)]):
-            leds.set_rgb(i, 250, 170, 0)
-            leds.update()
+        leds.set_all_rgb(0, 0, 0)
+        for i, petal in enumerate(self.petals):
+            if i == self.petalid:
+                if i != 0:
+                    for ledid in range(petal["leds"][0], petal["leds"][1]):
+                        leds.set_rgb(ledid, 250, 170, 0)
+                else:
+                    # top petal is a speshul kid
+                    for ledid in range(36, 40):
+                        leds.set_rgb(ledid, 250, 170, 0)
+                    for ledid in range(0, 5):
+                        leds.set_rgb(ledid, 250, 170, 0)
+
+        leds.update()
             
 
     def think(self, ins: InputState, delta_ms: int) -> None:
         super().think(ins, delta_ms)
-        for i in range(10):
-            petal = ins.captouch.petals[i]
-            if petal.pressed:
-                print(i)
+        # self.petalid = random.randint(0, 3)
+        # make all of the petals work
+        # wait for user input
+        # if user input matches the lit petal, move forward
+        for cap_index in range(10):
+            petal = ins.captouch.petals[cap_index]
+            if petal.pressed and cap_index == self.petals[self.petalid]["cap"]:
+                self.petalid = random.randint(0, 4)
+                print('jee')
 
 
-st3m.run.run_view(Pew(ApplicationContext()))
+# improvements
+# only wait input from the top petals
+
+st3m.run.run_view(Spede(ApplicationContext()))
